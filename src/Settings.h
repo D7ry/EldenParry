83a27cbaea949f3609c233b588bcf22014a2b588
ElectronicsArchiver <85485984+ElectronicsArchiver@users.noreply.github.com>
2022-07-10 21:39:37 -0400

    Refactored Settings

--- src/Settings.h
+++ src/Settings.h
@@ -1,70 +1,97 @@
 #pragma once
-static const char* settingsDir = "Data\\SKSE\\Plugins\\EldenParry.ini";
-class Settings
-{
-public:
-	class facts
-	{
+
+
+static const char * settingsDir = "Data\\SKSE\\Plugins\\EldenParry.ini";
+
+
+class Settings {
+	
 	public:
-		static inline bool isPrecisionAPIObtained = false;
-		static inline bool isValhallaCombatAPIObtained = false;
-	};
-	static inline float fParryTimeWindow = 0.2f;
-	static inline float fParryTimeWindow_Projectile = 1.0f;
-	static inline bool bEnableWeaponParry = true;
-	static inline bool bEnableShieldParry = true;
-	static inline bool bEnableNPCParry = true;
-	static inline bool bSuccessfulParryNoCost = true;
 
-	static inline bool bEnableSlowTimeEffect = false;
-	static inline bool bEnableScreenShakeEffect = true;
-	static inline bool bEnableParrySparkEffect = true;
-	static inline bool bEnableParrySoundEffect = true;
+		class facts {
+		public:
+		
+			static inline bool
+				isValhallaCombatAPIObtained = false ,
+				isPrecisionAPIObtained = false ;
+		};
+
+		static inline float 
+			fParryTimeWindow_Projectile = 1.0f ,
+			fProjectileParryExp = 20.0f ,
+			fParryTimeWindow = 0.2f ,
+			fMeleeParryExp = 10.0f ;
+		
+		static inline bool
+			bEnableArrowProjectileDeflection = true ,
+			bEnableMagicProjectileDeflection = true ,
+			bEnableScreenShakeEffect = true ,
+			bEnableParrySparkEffect = true ,
+			bEnableParrySoundEffect = true , 
+			bSuccessfulParryNoCost = true ,
+			bEnableSlowTimeEffect = false ,
+			bEnableWeaponParry = true ,
+			bEnableShieldParry = true ,
+			bEnableNPCParry = true ;
 
 
-	static inline bool bEnableArrowProjectileDeflection = true;
-	static inline bool bEnableMagicProjectileDeflection = true;
+		static void readSettings();
 
-	static inline float fProjectileParryExp = 20.0f;
-	static inline float fMeleeParryExp = 10.0f;
-
-	static void readSettings();
 
 	private:
-	static bool readSimpleIni(CSimpleIniA& a_ini, const char* a_iniAddress)
-	{
-		if (std::filesystem::exists(a_iniAddress)) {
-			a_ini.LoadFile(a_iniAddress);
-			return true;
-		} else {
-			logger::critical("{} is not a valid ini address.", a_iniAddress);
+
+		static bool readSimpleIni(
+			CSimpleIniA & settings ,
+			const char * path
+		){
+			if(std::filesystem::exists(path)){
+				settings.LoadFile(path);
+				return true;
+			}
+			
+			logger::critical("{} is not a valid ini address.",path);
 			return false;
 		}
-	}
 
-	static void ReadIntSetting(CSimpleIniA& a_ini, const char* a_sectionName, const char* a_settingName, uint32_t& a_setting)
-	{
-		const char* bFound = nullptr;
-		bFound = a_ini.GetValue(a_sectionName, a_settingName);
-		if (bFound) {
-			a_setting = static_cast<int>(a_ini.GetDoubleValue(a_sectionName, a_settingName));
-		}
-	}
-	static void ReadFloatSetting(CSimpleIniA& a_ini, const char* a_sectionName, const char* a_settingName, float& a_setting)
-	{
-		const char* bFound = nullptr;
-		bFound = a_ini.GetValue(a_sectionName, a_settingName);
-		if (bFound) {
-			a_setting = static_cast<float>(a_ini.GetDoubleValue(a_sectionName, a_settingName));
-		}
-	}
 
-	static void ReadBoolSetting(CSimpleIniA& a_ini, const char* a_sectionName, const char* a_settingName, bool& a_setting)
-	{
-		const char* bFound = nullptr;
-		bFound = a_ini.GetValue(a_sectionName, a_settingName);
-		if (bFound) {
-			a_setting = a_ini.GetBoolValue(a_sectionName, a_settingName);
+		static void ReadIntSetting(
+			CSimpleIniA & settings ,
+			const char * section ,
+			const char * setting,
+			uint32_t & value
+		){
+			const char * present = 
+				settings.GetValue(section,setting);
+			
+			if(present)
+				value = static_cast<int>(settings.GetDoubleValue(section,setting));
 		}
-	}
+
+
+		static void ReadFloatSetting(
+			CSimpleIniA & settings ,
+			const char * section ,
+			const char * setting ,
+			float & value
+		){
+			const char * present = 
+				settings.GetValue(section,setting);
+			
+			if(present)
+				value = static_cast<float>(settings.GetDoubleValue(section,setting));
+		}
+
+
+		static void ReadBoolSetting(
+			CSimpleIniA & settings ,
+			const char * section , 
+			const char * setting ,
+			bool & value
+		){
+			const char * present = 
+				settings.GetValue(section,setting);
+			
+			if(present)
+				value = settings.GetBoolValue(section,setting);
+		}
 };
