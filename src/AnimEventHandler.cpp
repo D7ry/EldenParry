4ae4cdcb8d719c4aaf19af5df0d16f949565f643
ElectronicsArchiver <85485984+ElectronicsArchiver@users.noreply.github.com>
2022-07-10 22:10:05 -0400

    Refactored Animation Event Handler

--- src/AnimEventHandler.cpp
+++ src/AnimEventHandler.cpp
@@ -1,32 +1,39 @@
 #include "AnimEventHandler.h"
 #include "EldenParry.h"
 
-constexpr uint32_t hash(const char* data, size_t const size) noexcept
-{
+
+constexpr uint32_t hash(const char * string,size_t const size) noexcept{
+
 	uint32_t hash = 5381;
 
-	for (const char* c = data; c < data + size; ++c) {
-		hash = ((hash << 5) + hash) + (unsigned char)*c;
-	}
+	for(const char * c = string;c < string + size;++c)
+		hash = ((hash << 5) + hash) + (unsigned char) * c;
 
 	return hash;
 }
 
-constexpr uint32_t operator"" _h(const char* str, size_t size) noexcept
-{
-	return hash(str, size);
+
+constexpr uint32_t operator"" _h(const char * string,size_t size) noexcept {
+	return hash(string,size);
 }
 
-RE::BSEventNotifyControl animEventHandler::HookedProcessEvent(RE::BSAnimationGraphEvent& a_event, RE::BSTEventSource<RE::BSAnimationGraphEvent>* src)
-{
-	FnProcessEvent fn = fnHash.at(*(uint64_t*)this);
-	//RE::ConsoleLog::GetSingleton()->Print(a_event.tag.c_str());
-	if (a_event.tag == "bashStop") {
-		if (a_event.holder) {
-			EldenParry::GetSingleton()->applyParryCost((RE::Actor*)a_event.holder);
-		}
-	}
-	return fn ? (this->*fn)(a_event, src) : RE::BSEventNotifyControl::kContinue;
+
+RE::BSEventNotifyControl animEventHandler::HookedProcessEvent(
+	RE::BSAnimationGraphEvent & event,
+	RE::BSTEventSource<RE::BSAnimationGraphEvent> * source
+){
+	
+	FnProcessEvent function = fnHash.at(* (uint64_t *) this);
+	
+	//RE::ConsoleLog::GetSingleton() -> Print(event.tag.c_str());
+
+	if(event.tag == "bashStop" && event.holder)
+		EldenParry::GetSingleton() -> applyParryCost((RE::Actor *) event.holder);
+	
+	return function 
+		? (this -> * function)(event,source) 
+		: RE::BSEventNotifyControl::kContinue ;
 }
 
-std::unordered_map<uint64_t, animEventHandler::FnProcessEvent> animEventHandler::fnHash;
+
+std::unordered_map<uint64_t,animEventHandler::FnProcessEvent> animEventHandler::fnHash;
--- src/AnimEventHandler.h
+++ src/AnimEventHandler.h
@@ -1,57 +1,82 @@
 #pragma once
+
+
 using EventResult = RE::BSEventNotifyControl;
-class animEventHandler
-{
-private:
-	template <class Ty>
-	static Ty SafeWrite64Function(uintptr_t addr, Ty data)
-	{
-		DWORD oldProtect;
-		void* _d[2];
-		memcpy(_d, &data, sizeof(data));
-		size_t len = sizeof(_d[0]);
 
-		VirtualProtect((void*)addr, len, PAGE_EXECUTE_READWRITE, &oldProtect);
-		Ty olddata;
-		memset(&olddata, 0, sizeof(Ty));
-		memcpy(&olddata, (void*)addr, len);
-		memcpy((void*)addr, &_d[0], len);
-		VirtualProtect((void*)addr, len, oldProtect, &oldProtect);
-		return olddata;
-	}
+class animEventHandler {
 
-	typedef RE::BSEventNotifyControl (animEventHandler::*FnProcessEvent)(RE::BSAnimationGraphEvent& a_event, RE::BSTEventSource<RE::BSAnimationGraphEvent>* dispatcher);
+	private:
 
-	RE::BSEventNotifyControl HookedProcessEvent(RE::BSAnimationGraphEvent& a_event, RE::BSTEventSource<RE::BSAnimationGraphEvent>* src);
+		typedef RE::BSEventNotifyControl 
+			(animEventHandler::* FnProcessEvent)
+			(RE::BSAnimationGraphEvent &,RE::BSTEventSource<RE::BSAnimationGraphEvent> *);
 
-	static void HookSink(uintptr_t ptr)
-	{
-		FnProcessEvent fn = SafeWrite64Function(ptr + 0x8, &animEventHandler::HookedProcessEvent);
-		fnHash.insert(std::pair<uint64_t, FnProcessEvent>(ptr, fn));
-	}
+		RE::BSEventNotifyControl HookedProcessEvent(
+			RE::BSAnimationGraphEvent & event ,
+			RE::BSTEventSource<RE::BSAnimationGraphEvent> * source);
 
-public:
-	/*Hook anim event sink*/
-	static void Register(bool player, bool NPC)
-	{
-		if (player) {
-			logger::info("Sinking animation event hook for player");
-			REL::Relocation<uintptr_t> pcPtr{ RE::VTABLE_PlayerCharacter[2] };
-			HookSink(pcPtr.address());
+
+		template <class Type>
+		static Type SafeWrite64Function(uintptr_t address,Type data){
+			
+			void * bytes[2];
+			memcpy(bytes,& data,sizeof(data));
+			
+			size_t length = sizeof(bytes[0]);
+
+			DWORD oldProtect;
+			VirtualProtect((void *) address,length,PAGE_EXECUTE_READWRITE,& oldProtect);
+			
+			Type old;
+			
+			memset(& old,0,sizeof(Type));
+			memcpy(& old,(void *) address,length);
+			memcpy((void *) address,& bytes[0],length);
+			
+			VirtualProtect((void *) address,length,oldProtect,& oldProtect);
+			
+			return old;
 		}
-		if (NPC) {
-			logger::info("Sinking animation event hook for NPC");
-			REL::Relocation<uintptr_t> npcPtr{ RE::VTABLE_Character[2] };
-			HookSink(npcPtr.address());
+
+
+		static void HookSink(uintptr_t pointer){
+			FnProcessEvent function = SafeWrite64Function(pointer + 0x8,& animEventHandler::HookedProcessEvent);
+			fnHash.insert(std::pair<uint64_t,FnProcessEvent>(pointer,function));
 		}
-		logger::info("Sinking complete.");
-	}
 
-	static void RegisterForPlayer()
-	{
-		Register(true, false);
-	}
 
-protected:
-	static std::unordered_map<uint64_t, FnProcessEvent> fnHash;
+	protected:
+
+		static std::unordered_map<uint64_t,FnProcessEvent> fnHash;
+
+
+	public:
+
+		/**
+		 *	@brief Hook animation event sink
+		 */
+
+		static void Register(bool isPlayer,bool isNPC){
+			
+			if(isPlayer){
+				logger::info("Sinking animation event hook for player");
+				REL::Relocation<uintptr_t> pointer { RE::VTABLE_PlayerCharacter[2] };
+				HookSink(pointer.address());
+				return;
+			}
+			
+			if (isNPC) {
+				logger::info("Sinking animation event hook for NPC");
+				REL::Relocation<uintptr_t> pointer { RE::VTABLE_Character[2] };
+				HookSink(pointer.address());
+				return;
+			}
+
+			logger::info("Sinking complete.");
+		}
+
+
+		static void RegisterForPlayer(){
+			Register(true,false);
+		}
 };
